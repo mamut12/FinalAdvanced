5b8e02f41b012bcf0472351c7b1db40bc9b173b7
Noushin <n01464164@humbermail.ca>
2021-01-18 19:49:34 -0500

    Question 18 - Generic

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp11
 {
+    public class Question18<T> where T:struct
+    {
+        private List<T> storage = new List<T>();
+
+        public void Add(T item)
+        {
+            storage.Add(item);
+        }
+
+        public T Get(int index)
+        {
+            return storage[index];
+        }
+
+        /*public List<T> Sorted()
+        {
+        
+        }*/
+    }
+
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Question18<int> q18 = new Question18<int>();
+            q18.Add(10);
+            q18.Add(20);
+            Console.WriteLine(q18.Get(0));
+
+
         }
     }
 }
